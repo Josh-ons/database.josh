0a34145bc2520d3bbba9f10bfca0dd38e5fdbd69
Josh-ons <joshonsongo@gmail.com>
2023-07-17 14:46:24 +0300

    initial commit

---
+++ Knee_gars/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import People
+from django.http import HttpResponse
+# Create your views here.
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def insert(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        school = request.POST.get('school')
+        email = request.POST.get('email')
+
+        person = People(name=name, school=school, email=email)
+        person.save()
+
+        # print(name,school,email)
+    return HttpResponse("Success")
+
+
+def people(request):
+    d = People.objects.all()
+
+    context={"d": d}
+    return render(request, "people.html", context)
+
